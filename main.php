010a1a11e90c9bf3eeb127b90a8fc6458a16b8ae
Basti <51531217+crftwrk@users.noreply.github.com>
2022-02-21 12:51:08 +0100

    Update FA6 icons HTML

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 /*Plugin Name: bS Share Buttons
 Plugin URI: https://bootscore.me/plugins/bs-share-buttons/
 Description: Share Buttons for bootScore theme https://bootscore.me. Use Shortcode [bs-share-buttons] to display buttons in content or widget. Use <?php echo do_shortcode("[bs-share-buttons]"); ?&gt; to display in .php files.
-Version: 5.0.0
+Version: 5.2.0
 Author: bootScore
 Author URI: https://bootscore.me
 License: MIT License
@@ -70,18 +70,18 @@ function bs_share_buttons($content) {
 
         // Add sharing button at the end of page/page content
         $content .= '<div id="share-buttons" class="mb-3">';
-        $content .= '<a class="mb-1 btn btn-sm btn-twitter" title="Twitter" href="'. $twitterURL .'" target="_blank" rel="nofollow"><i class="fab fa-twitter"></i></a> ';
-        $content .= '<a class="mb-1 btn btn-sm btn-facebook" title="Facebook" href="'.$facebookURL.'" target="_blank" rel="nofollow"><i class="fab fa-facebook-f"></i></a> ';
-        $content .= '<a class="mb-1 btn btn-sm btn-whatsapp" title="Whatsapp" href="'.$whatsappURL.'" target="_blank" rel="nofollow"><i class="fab fa-whatsapp"></i></a> ';
-        $content .= '<a class="mb-1 btn btn-sm btn-pinterest" title="Pinterest" href="'.$pinterestURL.'" data-pin-custom="true" target="_blank" rel="nofollow"><i class="fab fa-pinterest-p"></i></a> ';
-        $content .= '<a class="mb-1 btn btn-sm btn-linkedin" title="LinkedIn" href="'.$linkedInURL.'" target="_blank" rel="nofollow"><i class="fab fa-linkedin-in"></i></a> ';
-        $content .= '<a class="mb-1 btn btn-sm btn-reddit" title="Reddit" href="'.$redditURL.'" target="_blank" rel="nofollow"><i class="fab fa-reddit-alien"></i></a> '; 
-        $content .= '<a class="mb-1 btn btn-sm btn-tumblr" title="Tumblr" href="'.$tumblrURL.'" target="_blank" rel="nofollow"><i class="fab fa-tumblr"></i></a> ';
+        $content .= '<a class="mb-1 btn btn-sm btn-twitter" title="Twitter" href="'. $twitterURL .'" target="_blank" rel="nofollow"><i class="fa-brands fa-twitter"></i></a> ';
+        $content .= '<a class="mb-1 btn btn-sm btn-facebook" title="Facebook" href="'.$facebookURL.'" target="_blank" rel="nofollow"><i class="fa-brands fa-facebook-f"></i></a> ';
+        $content .= '<a class="mb-1 btn btn-sm btn-whatsapp" title="Whatsapp" href="'.$whatsappURL.'" target="_blank" rel="nofollow"><i class="fa-brands fa-whatsapp"></i></a> ';
+        $content .= '<a class="mb-1 btn btn-sm btn-pinterest" title="Pinterest" href="'.$pinterestURL.'" data-pin-custom="true" target="_blank" rel="nofollow"><i class="fa-brands fa-pinterest-p"></i></a> ';
+        $content .= '<a class="mb-1 btn btn-sm btn-linkedin" title="LinkedIn" href="'.$linkedInURL.'" target="_blank" rel="nofollow"><i class="fa-brands fa-linkedin-in"></i></a> ';
+        $content .= '<a class="mb-1 btn btn-sm btn-reddit" title="Reddit" href="'.$redditURL.'" target="_blank" rel="nofollow"><i class="fa-brands fa-reddit-alien"></i></a> '; 
+        $content .= '<a class="mb-1 btn btn-sm btn-tumblr" title="Tumblr" href="'.$tumblrURL.'" target="_blank" rel="nofollow"><i class="fa-brands fa-tumblr"></i></a> ';
         $content .= '<a class="mb-1 btn btn-sm btn-buffer" title="Buffer" href="'.$bufferURL.'" target="_blank" rel="nofollow"><i class="fab fa-buffer"></i></a> ';
-        $content .= '<a class="mb-1 btn btn-sm btn-mix" title="mix" href="'.$mixURL.'" target="_blank" rel="nofollow"><i class="fab fa-mix"></i></a> ';
-        $content .= '<a class="mb-1 btn btn-sm btn-vk" title="vk" href="'.$vkURL.'" target="_blank" rel="nofollow"><i class="fab fa-vk"></i></a> ';
-        $content .= '<a class="mb-1 btn btn-sm btn-mail btn-dark" title="Mail" href="'.$mailURL.'"><i class="fas fa-envelope"></i></a> ';
-        $content .= '<a class="mb-1 btn btn-sm btn-print btn-dark" title="Print" href="javascript:;" onclick="window.print()"><i class="fas fa-print"></i></a>';
+        $content .= '<a class="mb-1 btn btn-sm btn-mix" title="mix" href="'.$mixURL.'" target="_blank" rel="nofollow"><i class="fa-brands fa-mix"></i></a> ';
+        $content .= '<a class="mb-1 btn btn-sm btn-vk" title="vk" href="'.$vkURL.'" target="_blank" rel="nofollow"><i class="fa-brands fa-vk"></i></a> ';
+        $content .= '<a class="mb-1 btn btn-sm btn-mail btn-dark" title="Mail" href="'.$mailURL.'"><i class="fa-solid fa-envelope"></i></a> ';
+        $content .= '<a class="mb-1 btn btn-sm btn-print btn-dark" title="Print" href="javascript:;" onclick="window.print()"><i class="fa-solid fa-print"></i></a>';
         $content .= '</div>';
         
         return $content;
